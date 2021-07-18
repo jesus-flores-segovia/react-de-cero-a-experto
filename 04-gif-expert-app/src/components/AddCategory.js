b2f17feaaf118cd6e949ce9fafbbe9b61c6346c6
Jesús <Jess@LAPTOP-QRSS98AU>
2021-07-18 21:24:10 +0200

    GifExpertApp. Refactor: AddCategory component adjusted to render as first

--- 04-gif-expert-app/src/components/AddCategory.js
+++ 04-gif-expert-app/src/components/AddCategory.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 export const AddCategory = ({setCategories}) => {
     
     // Es importante inicializar el state, para evitar el warning que aparece por consola, que nos avisa de cambios incontrolados en el input
-    const [inputValue, setInputValue] = useState();
+    const [inputValue, setInputValue] = useState("");
     
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
@@ -14,7 +14,7 @@ export const AddCategory = ({setCategories}) => {
         e.preventDefault();
         if(inputValue.trim().length !== 0){
             // Recibimos las categories en el callback de la función del useState
-            setCategories(categories => [...categories, inputValue]);
+            setCategories(categories => [inputValue, ...categories]);
             setInputValue("");
         }
         
