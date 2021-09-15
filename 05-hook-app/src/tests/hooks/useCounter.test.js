7a102f948f18a93fc1bb14880725d4f2643f84d0
Jesús <Jess@LAPTOP-QRSS98AU>
2021-09-15 23:14:09 +0200

    HookApp. Test: added tests for useCounter hook

--- 05-hook-app/src/tests/hooks/useCounter.test.js
+++ 05-hook-app/src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import {renderHook} from "@testing-library/react-hooks";
+import {act, renderHook} from "@testing-library/react-hooks";
 import {useCounter} from "../../hooks/useCounter";
 
 describe("Tests inside file useCounter.test.js", () => {
@@ -10,6 +10,40 @@ describe("Tests inside file useCounter.test.js", () => {
         expect(typeof result.current.increment).toBe("function");
         expect(typeof result.current.decrement).toBe("function");
         expect(typeof result.current.reset).toBe("function");
+    });
+
+    test("'useCounter' increment function must increment the value in one", () => {
+        const {result} = renderHook(() => useCounter(100));
+        const {increment} = result.current;
+
+        act(() => {
+            increment(1);
+        });
+
+        expect(result.current.counter).toBe(101);
+    });
+
+    test("'useCounter' decrement function must decrement the value in one", () => {
+        const {result} = renderHook(() => useCounter(100));
+        const {decrement} = result.current;
+
+        act(() => {
+            decrement(1);
+        });
+
+        expect(result.current.counter).toBe(99);
+    });
+
+    test("'useCounter' reset function must reset the state value of hook", () => {
+        const {result} = renderHook(() => useCounter(100));
+        const {decrement, reset} = result.current;
+
+        act(() => {
+            decrement(1);
+            reset();
+        });
+
+        expect(result.current.counter).toBe(100);
     });
 
 });
