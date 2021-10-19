e46f32fd0c9e13b6e07bf210d898e6d753b30367
Jesús <Jess@LAPTOP-QRSS98AU>
2021-10-19 23:40:08 +0200

    HeroesApp. Refactor: changed a name of a route path

--- 06-heroes-app/src/routers/DashboardRoutes.js
+++ 06-heroes-app/src/routers/DashboardRoutes.js
@@ -16,7 +16,7 @@ export const DashboardRoutes = () => {
             <div className="container mt-5">
                 <Switch>
                     <Route exact path="/marvel" component={MarvelScreen}/>
-                    <Route exact path="/heroe/:heroeId" component={HeroScreen}/>
+                    <Route exact path="/hero/:heroId" component={HeroScreen}/>
                     <Route exact path="/dc" component={DcScreen}/>
 
                     <Redirect to="/marvel"/>
