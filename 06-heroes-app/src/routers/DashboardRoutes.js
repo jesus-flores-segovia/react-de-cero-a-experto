ee978b6f50fdae3d31fa034b3e1c13a8d6f9c2b2
Jesús <Jess@LAPTOP-QRSS98AU>
2021-10-23 14:02:24 +0200

    HeroesApp. Feature: added the search route to the router

--- 06-heroes-app/src/routers/DashboardRoutes.js
+++ 06-heroes-app/src/routers/DashboardRoutes.js
@@ -8,6 +8,7 @@ import { HeroScreen } from '../components/heroes/HeroScreen';
 import { DcScreen } from "../components/dc/DcScreen";
 import { MarvelScreen } from "../components/marvel/MarvelScreen";
 import { Navbar } from '../components/ui/NavBar';
+import { SearchScreen } from '../components/search/SearchScreen';
 
 export const DashboardRoutes = () => {
     return (
@@ -18,6 +19,7 @@ export const DashboardRoutes = () => {
                     <Route exact path="/marvel" component={MarvelScreen}/>
                     <Route exact path="/hero/:heroId" component={HeroScreen}/>
                     <Route exact path="/dc" component={DcScreen}/>
+                    <Route exact path="/search" component={SearchScreen}/>
 
                     <Redirect to="/marvel"/>
                 </Switch>
