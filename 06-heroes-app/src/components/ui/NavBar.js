36b5c3731db3295601caba1a0445ebb076f8c327
Jesús <Jess@LAPTOP-QRSS98AU>
2021-10-23 14:01:01 +0200

    HeroesApp. Refactor: NavBar styles and behaviour

--- 06-heroes-app/src/components/ui/NavBar.js
+++ 06-heroes-app/src/components/ui/NavBar.js
@@ -1,52 +1,63 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand ms-2" href="/">
+        <img className="me-2" src="/assets/icons/icons8-hulk-48.png" width="30" height="30" alt="heroes"/>
+            Heroes
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <NavLink 
+            activeClassName="active"
+            className="nav-link" 
+            exact
+            to="/marvel"
+        >
+            Marvel Comics
+        </NavLink>    
+
+        <NavLink 
+            activeClassName="active"
+            className="nav-link" 
+            exact
+            to="/dc"
+        >
+            DC Comics
+        </NavLink>
+
+        <NavLink 
+            activeClassName="active"
+            className="nav-link" 
+            exact
+            to="/search"
+        >
+            Search
+        </NavLink>
+      </ul>
+    </div>
+    <div className="collapse navbar-collapse justify-content-end" id="navbarText">
+      <ul className="navbar-nav mb-2 mb-lg-0">
+        <button type="button" className="btn btn-danger">
+            <NavLink 
+                activeClassName="active"
+                className="nav-link" 
+                exact
+                to="/logout"
+                style={{color:"white"}}
             >
-                Asociaciones
-            </Link>
-
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/marvel"
-                    >
-                        Marvel
-                    </NavLink>
-
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
-                </div>
-            </div>
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
-                    >
-                        Logout
-                    </NavLink>
-                </ul>
-            </div>
-        </nav>
+                Logout
+            </NavLink>
+            </button>
+      </ul>
+    </div>
+  </div>
+</nav>
     )
 }
