398b9cefa26cca8f7d617a4eaf719d067bc99274
Jesús <Jess@LAPTOP-QRSS98AU>
2021-10-26 20:54:01 +0200

    HeroesApp. Feature: added queryString functionalities with the aim to read the query params of url

--- 06-heroes-app/src/components/search/SearchScreen.js
+++ 06-heroes-app/src/components/search/SearchScreen.js
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { heroes } from '../../data/heroes'
+import React, { useMemo } from 'react';
+import queryString from 'query-string';
 import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../heroes/HeroCard';
+import { useLocation } from 'react-router';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
-export const SearchScreen = () => {
+export const SearchScreen = ({history}) => {
 
-    const [filteredHeroes, setFilteredHeroes] = useState([]);
+    const location = useLocation();
+    const q = queryString.parse(location.search).q;
 
     const [values, handleInputChange] = useForm({
-        query: ""
+        query: q || ""
     });
 
     const {query} = values;
 
+    let filteredHeroes = useMemo(() => {
+        return getHeroesByName(q);
+    }, [q]);
+
     const handleSearch = (e) => {
         e.preventDefault();
-        setFilteredHeroes(
-            heroes.filter((hero) => (hero.superhero.toLowerCase().includes(query.toLowerCase())) )
-        );        
+
+        if (!query) {
+            history.replace({ pathname: location.pathname, search: ''});
+            return;
+        }
+             
+        location.search = queryString.stringify({q: query});
+        history.push(`?${location.search}`);    
     }
-
-    console.log(filteredHeroes)
-
-    useEffect(() => {
-        console.log("Component re-render")
-    }, [filteredHeroes])
-
 
     return (
         <div>
@@ -51,7 +56,7 @@ export const SearchScreen = () => {
                         />
                         <button 
                             type="submit"
-                            className="btn btn-block btn-outline-primary m-1"
+                            className="btn btn-block btn-outline-primary w-100 mt-2"
                         >
                             Search
                         </button>
@@ -61,9 +66,28 @@ export const SearchScreen = () => {
                 <div className="col-7">
                     <h4>Results</h4>
                     <hr/>
+                    {
+                        filteredHeroes.length === 0 && 
+                        !q &&
+                            <div className="alert alert-info" role="alert">
+                                <p>
+                                    Try to search something.
+                                </p>
+                            </div>
+                    }
+                    {
+                        filteredHeroes.length === 0 && 
+                        q &&
+                            <div className="alert alert-danger" role="alert">
+                                <p>
+                                    There are not results for '{query}'. <br/>
+                                    Please try again.
+                                </p>
+                            </div>
+                    }
                     <div className="row">
                         {
-                            filteredHeroes.map(hero => 
+                            filteredHeroes.length !== 0 && filteredHeroes.map(hero => 
                                 <HeroCard 
                                     key={hero.id}
                                     {...hero}
