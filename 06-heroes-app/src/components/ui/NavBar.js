049172a2da1f32900ab39016cbc1c2821abe4da8
Jesús <Jess@LAPTOP-QRSS98AU>
2021-10-29 20:20:56 +0200

    HeroesApp. Feature: user logged retrieved to show user information

--- 06-heroes-app/src/components/ui/NavBar.js
+++ 06-heroes-app/src/components/ui/NavBar.js
@@ -1,63 +1,69 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { AuthContext } from '../../auth/AuthContext'
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand ms-2" href="/">
-        <img className="me-2" src="/assets/icons/icons8-hulk-48.png" width="30" height="30" alt="heroes"/>
-            Heroes
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarText">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <NavLink 
-            activeClassName="active"
-            className="nav-link" 
-            exact
-            to="/marvel"
-        >
-            Marvel Comics
-        </NavLink>    
 
-        <NavLink 
-            activeClassName="active"
-            className="nav-link" 
-            exact
-            to="/dc"
-        >
-            DC Comics
-        </NavLink>
+  const {user} = useContext(AuthContext);
 
-        <NavLink 
-            activeClassName="active"
-            className="nav-link" 
-            exact
-            to="/search"
-        >
-            Search
-        </NavLink>
-      </ul>
-    </div>
-    <div className="collapse navbar-collapse justify-content-end" id="navbarText">
-      <ul className="navbar-nav mb-2 mb-lg-0">
-        <button type="button" className="btn btn-danger">
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand ms-2" href="/">
+          <img className="me-2" src="/assets/icons/icons8-hulk-48.png" width="30" height="30" alt="heroes"/>
+          Heroes
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <NavLink 
                 activeClassName="active"
                 className="nav-link" 
                 exact
-                to="/logout"
-                style={{color:"white"}}
+                to="/marvel"
             >
-                Logout
+                Marvel Comics
+            </NavLink>    
+
+            <NavLink 
+                activeClassName="active"
+                className="nav-link" 
+                exact
+                to="/dc"
+            >
+                DC Comics
             </NavLink>
-            </button>
-      </ul>
+
+            <NavLink 
+                activeClassName="active"
+                className="nav-link" 
+                exact
+                to="/search"
+            >
+                Search
+            </NavLink>
+          </ul>
+      </div>
+      <div className="collapse navbar-collapse justify-content-end" id="navbarText">
+        <ul className="navbar-nav mb-2 mb-lg-0 align-items-center">
+        {user.name &&
+          <span className="nav-item text-info me-3">You're logged as {user.name}</span>
+        }
+          <button type="button" className="btn btn-danger">
+              <NavLink 
+                  activeClassName="active"
+                  exact
+                  to="/login"
+                  style={{color:"white", textDecoration:"none"}}
+              >
+                  Logout
+              </NavLink>
+          </button>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-    )
+  </nav>
+  )
 }
