fcd6c1012c2ab0f195d2bbaf7baf5df30ff19a34
Jesús <Jess@LAPTOP-QRSS98AU>
2021-12-28 21:44:27 +0100

    JournalApp. Feature: added notes screens

---
+++ 07-journal-app/src/components/notes/NotesAppBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+    return (
+        <div className="notes__appbar">
+            <span>28th August 2021</span>
+            <div>
+                <button className="buttons__btn">
+                    Picture
+                </button>
+                <button className="buttons__btn">
+                    Save
+                </button>
+            </div>
+        </div>
+    )
+}
