0656026768e6535d03a57a29dff7edfc2a36a510
Jesús <Jess@LAPTOP-QRSS98AU>
2022-01-10 19:27:24 +0100

    JournalApp. Feature: added handlers to manage the note save and file upload

--- 07-journal-app/src/components/notes/NotesAppBar.js
+++ 07-journal-app/src/components/notes/NotesAppBar.js
@@ -1,14 +1,47 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 export const NotesAppBar = () => {
+
+    const {active: note} = useSelector(state => state.notes);
+    const dispatch = useDispatch();
+
+    const handleSave = () => {
+        dispatch(startSaveNote(note));
+    }
+
+    const handlePictureUpload = () => {
+        document.querySelector("#fileSelector").click();
+    }
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+
+        if(file) {
+            dispatch(startUploading(file));
+        }
+    }
+
     return (
         <div className="notes__appbar">
             <span>28th August 2021</span>
+            <input
+                id='fileSelector' 
+                type="file" 
+                style={{display: 'none'}}
+                onChange={handleFileChange}    
+                />
             <div>
-                <button className="buttons__btn">
+                <button 
+                    className="buttons__btn"
+                    onClick={handlePictureUpload}>
                     Picture
                 </button>
-                <button className="buttons__btn">
+                <button 
+                    className="buttons__btn"
+                    onClick={handleSave}
+                    >
                     Save
                 </button>
             </div>
