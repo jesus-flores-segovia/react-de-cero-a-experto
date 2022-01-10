dd07e34ad1978d7024537b9ef08e10eecc12bec7
Jesús <Jess@LAPTOP-QRSS98AU>
2022-01-10 19:21:42 +0100

    JournalApp. Feature: added all types needed to handle notes

--- 07-journal-app/src/types/types.js
+++ 07-journal-app/src/types/types.js
@@ -6,4 +6,12 @@ export const types = {
     uiRemoveError: '[UI] Remove Error',
     uiStartLoading: '[UI] Start Loading',
     uiFinishLoading: '[UI] Finish Loading',
+
+    notesAddNew: '[Notes] New Note',
+    notesActive: '[Notes] Set active note',
+    notesLoad: '[Notes] Load notes',
+    notesUpdated: '[Notes] Updated note',
+    notesFileUrl: '[Notes] Updated image url',
+    notesDelete: '[Notes] Delete note',
+    notesLogoutCleaning: '[Notes] Logout cleaning',
 }
