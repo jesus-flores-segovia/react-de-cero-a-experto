317b08c95327e340a26fde2ff1c00b54b3d3fb68
Jesús <Jess@LAPTOP-QRSS98AU>
2022-01-10 19:28:05 +0100

    JournalApp. Feature: added the note form and delete handler

--- 07-journal-app/src/components/notes/NoteScreen.js
+++ 07-journal-app/src/components/notes/NoteScreen.js
@@ -1,22 +1,66 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setActiveNote, startDelete } from '../../actions/notes';
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const {active: note} = useSelector(state => state.notes);
+    const [values, handleInputChange, reset] = useForm(note);
+    const {title, body} = values;
+
+    const activeId = useRef(note.id);
+
+    useEffect(() => {
+        if(note.id !== activeId.current) {
+            reset(note);
+            activeId.current = note.id;
+        }
+    }, [note, reset])
+
+    useEffect(() => {
+        dispatch(setActiveNote(values.id, {imageUrl: note.imageUrl, ...values}));
+    }, [values, dispatch])
+
+    const handleDelete = () => {
+        dispatch(startDelete(note.id));
+    }
+    
     return (
         <div className="notes__main-content">
             <NotesAppBar/>
             <div className="notes__content">
-                <input type="text"
-                placeholder="Some awesome title"
-                className="notes__title-input"
-                autoComplete="off"
+                <input 
+                    name='title'
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                    value={title}
+                    onChange={handleInputChange}
                 />
-                <textarea placeholder="What happened today"
-                className="notes__textarea"></textarea>
-                <div className="notes__image">
-                    <img src="https://whatsup.es/wp-content/uploads/2020/09/travel-trip-journey.jpg" alt="image"/>
-                </div>
+                <textarea 
+                    name='body'
+                    placeholder="What happened today"
+                    className="notes__textarea" 
+                    value={body}
+                    onChange={handleInputChange}    
+                />
+                {note.imageUrl &&
+                    <div className="notes__image">
+                        <img src={note.imageUrl} alt="image"/>
+                    </div>
+                }
             </div>
+            <button 
+                className='buttons__btn buttons__btn-danger'
+                onClick={handleDelete}
+                >
+                Delete
+            </button>
         </div>
     )
 }
