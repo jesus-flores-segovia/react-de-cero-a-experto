ad9a211f09e5f2308f8be2d27ec3d357dac5b2f0
Jesús <Jess@LAPTOP-QRSS98AU>
2022-01-10 19:22:23 +0100

    JournalApp. Feature: added handler for new notes and logout

--- 07-journal-app/src/components/journal/Sidebar.js
+++ 07-journal-app/src/components/journal/Sidebar.js
@@ -1,28 +1,40 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { startLogout } from '../../actions/auth';
+import { startNewNote } from '../../actions/notes';
 import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
 
     const dispatch = useDispatch();
 
+    const {name} = useSelector(state => state.auth);
+
     const handleLogout = () => {
         dispatch(startLogout());
     };
+
+    const handleAddNew = () => {
+        dispatch(startNewNote());
+    }
 
     return (
         <aside className="journal__sidebar">
             <div className="journal__sidebar-navbar">
                 <h3 className="mt-5">
                     <i className="far fa-moon" />
-                    <span> Jesús</span>
+                    <span> {name}</span>
                 </h3>
-                <button className="buttons__btn" onClick={handleLogout}>
+                <button 
+                    className="buttons__btn" 
+                    onClick={handleLogout}
+                    >
                     Logout
                 </button>
             </div>
-            <div className="journal__new-entry">
+            <div className="journal__new-entry"
+                onClick={handleAddNew}>
                 <i className="far fa-calendar-plus fa-5x" />
                 <p className="mt-5">New Entry</p>
             </div>
