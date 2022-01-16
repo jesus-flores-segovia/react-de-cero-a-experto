ad61fcafcf080c93caaaaa29171ec540f96e773e
Jesús <Jess@LAPTOP-QRSS98AU>
2022-01-16 20:08:40 +0100

    JournalApp. Feature: added firebase config from Nodejs environment variables

--- 07-journal-app/src/firebase/firebaseConfig.js
+++ 07-journal-app/src/firebase/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore/lite';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9YBlbLQ59pHNqmQO8e_sZvOLm5pQoUiY",
-  authDomain: "journal-app-b2ec2.firebaseapp.com",
-  projectId: "journal-app-b2ec2",
-  storageBucket: "journal-app-b2ec2.appspot.com",
-  messagingSenderId: "1032005234284",
-  appId: "1:1032005234284:web:ad1d38368c71df95fab249",
-  measurementId: "G-J007L6QS82"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
